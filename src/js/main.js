20973c548ba063b037ba579ef2a01f99f296239b
schmooblidon <willzyx123@googlemail.com>
2019-04-08 21:29:57 +0100

    iframe compatible

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,19 @@ import Game from "./game";
 import { setupLayers } from "./draw/draw";
 import { setupControlsBox, setupControls } from "./controls";
 
-export let curGame = new Game(slp_replay);
+export let curGame = null;
 
 export let displayDebug = false;
 
-function start (){
+function start (slp_replay){
+  curGame = new Game(slp_replay);
+
+  setupLayers();
+  resize();
+  //setupControlsBox();
+  setupControls();
 
   curGame.playback.start();
 }
 
 window.start = start;
-
-setupLayers();
-resize();
-setupControlsBox();
-setupControls();
